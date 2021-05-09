97497e8056b682469c588c4e0ce8d6b8446d95be
FlutLab.io <>
2021-05-09 15:20:05 +0000

    Autosave

---
+++ lib/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+          appBar: AppBar(
+            title: Text('Login'),
+          ),
+          body: new Container(
+            child: new Text('data')
+          )
+          
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,22 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+import 'login_page.dart';
+void main()
+{
+  runApp(new Myapp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({@required this.title});
-
+class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    return new MaterialApp(
+      title: 'Login',
+      theme: new ThemeData(
+        primarySwatch: Colors.bleu,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+      home: new LoginPage()
+    )
+ 
+
+    
   }
 }
