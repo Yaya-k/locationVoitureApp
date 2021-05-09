54a6405cdd2b8123d0465c11e24a138f247bb34e
FlutLab.io <>
2021-05-09 15:59:56 +0000

    log

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,36 +4,36 @@ class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _LoginPageState();
 }
-class _LoginPageState extends State<LoginPage>{
+
+class _LoginPageState extends State<LoginPage> {
   String _email;
   String _password;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-          appBar: AppBar(
-            title: Text('Login'),
-          ),
-          body: new Container(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: new Container(
             Padding: EdgeInsets.all(16.0),
             child: new Form(
-              child: new Column(
-                children: <Widget>[
-                  new TextFormField(
-                    decoration: new InputDecoration(labelText: 'Email', )
-                  ),//textFormField
-                  new TextFormField(
-                    decoration: new InputDecoration(labelText: 'Password', 
+                child: new Column(
+              children: <Widget>[
+                new TextFormField(
+                    decoration: new InputDecoration(
+                  labelText: 'Email',
+                )), //textFormField
+                new TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: 'Password',
+                  ), // InputDecoration
+                  obscureText: true,
+                ), // textFormField
+              ], //<widget>
+            ) //colums
+                ) //form
+            ) //container
 
-                    ),// InputDecoration
-                    obscureText: true,
-
-                  ),// textFormField
-
-                ],//<widget>
-              )//colums
-            )//form
-          ) //container
-         
         );
   }
 }
